f55a042d80b7fea982984dabf7b20a1ab9962240
Anwesh43 <anweshthecool0@gmail.com>
2020-08-25 01:43:37 +0530

    adding Renderer class

--- wifisignalsinifiedview/src/main/java/com/anwesh/uiprojects/wifisignalsinifiedview/WifiSignalSinifiedView.kt
+++ wifisignalsinifiedview/src/main/java/com/anwesh/uiprojects/wifisignalsinifiedview/WifiSignalSinifiedView.kt
@@ -185,4 +185,28 @@ class WifiSignalSinifiedView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : WifiSignalSinifiedView) {
+
+        private val animator : Animator = Animator(view)
+        private val wss : WifiSinifiedSignal = WifiSinifiedSignal(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            wss.draw(canvas, paint)
+            animator.animate {
+                wss.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            wss.startUpdating {
+                animator.start()
+            }
+        }
+
+    }
 }
